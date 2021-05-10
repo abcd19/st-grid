3fa7b28930e24f6e09174f0dc80606fdfc3a3804
abcd19 <artm000@mail.ru>
2021-05-11 00:42:25 +0300

    made test for grid

--- src/DirectoryTbl/DirectoryTbl.tsx
+++ src/DirectoryTbl/DirectoryTbl.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
-import {GridLayout, IToolbarLayoutProps, typeColumn, typeItem, tyepCellVal} from './../Grid'
-import {onChangeItem, onMouseDownItem, onClickHeaderCell} from './DirectoryTblHandle'
-import {sortItems} from './Sorting/sortItems';
+import { GridLayout, IToolbarLayoutProps, typeColumn, typeItem, tyepCellVal } from './../Grid'
+import { onChangeItem, onMouseDownItem, onClickHeaderCell } from './DirectoryTblHandle'
+import { sortItems } from './Sorting/sortItems';
 import * as ST from '../common'
-import {createDirectoryTableToolbar} from './Toolbar/createDirectoryTableToolbar';
+import { createDirectoryTableToolbar } from './Toolbar/createDirectoryTableToolbar';
 
 export interface IDirectoryTblProps {
-  height?: number; 
+  height?: number;
   width?: number;
   sortingFlag?: boolean;
   items?: typeItem[];
   addBtnFlag?: boolean;
   removeBtnFlag?: boolean;
   removeAllBtnFlag?: boolean;
-  onRemoveAllItems?: ()=>void;
-  onSelectItem?: (item?:typeItem, num?: number)=>void;
-  onChange?: (newItems: typeItem[], obj: {event: string,  removedItem?: typeItem, selItemNum?: number, cellAlias?: string, newVal?: tyepCellVal})=> void;
+  onMouseEnterItem?: (rowObject: typeItem, cellAlias: string) => void,
+  onMouseLeaveItem?: (rowObject: typeItem, cellAlias: string) => void,
+  onDoubleClickItem?: (rowObject: typeItem, cellAlias: string) => void;
+  onRemoveAllItems?: () => void;
+  onSelectItem?: (item?: typeItem, num?: number) => void;
+  onChange?: (newItems: typeItem[], obj: { event: string, removedItem?: typeItem, selItemNum?: number, cellAlias?: string, newVal?: tyepCellVal }) => void;
   columns: typeColumn[];
 }
 
@@ -24,14 +27,14 @@ export interface IDirectoryTblState {
   scrollToLastItem: boolean;
   sorting?: {
     order: string | undefined,
-    cellAlias: string; 
+    cellAlias: string;
   };
   selItemNum?: number;
 }
 
 export class DirectoryTbl extends React.Component<IDirectoryTblProps, IDirectoryTblState>{
 
-  static defaultProps  = {
+  static defaultProps = {
     height: 200,
     width: 400,
     sortingFlag: true,
@@ -39,17 +42,16 @@ export class DirectoryTbl extends React.Component<IDirectoryTblProps, IDirectory
   };
 
   private onChangeItem: (rowObject: typeItem, cellAlias: string, val: tyepCellVal) => void;
-  public onChange: ()=>void = ()=>{/* do nothing */};
-  public onRemoveBtnClick: ()=>void = ()=>{/* do nothing */};
-  public onAddBtnClick: ()=>void = ()=>{/* do nothing */};
+  public onChange: () => void = () => {/* do nothing */ };
+  public onRemoveBtnClick: () => void = () => {/* do nothing */ };
+  public onAddBtnClick: () => void = () => {/* do nothing */ };
   private onMouseDownItem: (rowObject: typeItem, cellAlias: string) => void;
   private onClickHeaderCell: (sortingCellAlias: string, orderSorting?: string) => void;
   private toolbar: IToolbarLayoutProps
 
-  constructor(props: IDirectoryTblProps)
-  {
+  constructor(props: IDirectoryTblProps) {
     super(props);
-    
+
     this.onChangeItem = onChangeItem.bind(this);
     this.onMouseDownItem = onMouseDownItem.bind(this);
     this.onClickHeaderCell = onClickHeaderCell.bind(this);
@@ -64,54 +66,56 @@ export class DirectoryTbl extends React.Component<IDirectoryTblProps, IDirectory
     }
 
   }
-  
 
-  render(): React.ReactElement
-  {
-    const {height = 200, width = 400, columns, sortingFlag = true} = this.props;
-    const {scrollToLastItem, sorting, selItemNum} = this.state;
-    let {items = []} = this.props
 
-    for(const item of items)
-    {
+  render(): React.ReactElement {
+
+    const { height = 200, width = 400, columns, sortingFlag = true } = this.props;
+    const {
+      onMouseEnterItem = () => {/* do nothing */ },
+      onMouseLeaveItem = () => {/* do nothing */ },
+      onDoubleClickItem = () => {/* do nothing */ }
+    } = this.props;
+
+    const { scrollToLastItem, sorting, selItemNum } = this.state;
+    let { items = [] } = this.props
+
+    for (const item of items) {
       item.color = undefined;
       item.layoutMode = 'view';
     }
 
 
-    if(typeof(selItemNum) == 'number')
-    {
+    if (typeof (selItemNum) == 'number') {
 
-      if(ST.isObject(items[ selItemNum ]) )
-      {
+      if (ST.isObject(items[selItemNum])) {
 
-        items[ selItemNum ].color = '#EDF5FC';
-        items[ selItemNum ].layoutMode = 'edit';
+        items[selItemNum].color = '#EDF5FC';
+        items[selItemNum].layoutMode = 'edit';
       }
 
     }
 
-    if(sorting && ST.isString(sorting.order))
-    {
+    if (sorting && ST.isString(sorting.order)) {
       items = sortItems(ST.clone(items), sorting.cellAlias, sorting.order);
     }
 
 
 
-    return <GridLayout   
-      scrollToLastItem = {scrollToLastItem}
-      width = {width} 
-      height = {height} 
-      onChangeItem = {this.onChangeItem}
-      onMouseDownItem = {this.onMouseDownItem}
-      onClickHeaderCell ={this.onClickHeaderCell}
-      onMouseEnterItem = {()=>{/* do nothing */}}
-      onMouseLeaveItem = {()=>{/* do nothing */}}
-      onDoubleClickItem = {()=>{ /* do nothing */}}
-      onClickItem = {()=>{/* do nothing */}}
-      sortingFlag = {sortingFlag}
-      toolbar = {this.toolbar}
-      items = {items}
-      columns = {columns}/>
+    return <GridLayout
+      scrollToLastItem={scrollToLastItem}
+      width={width}
+      height={height}
+      onChangeItem={this.onChangeItem}
+      onMouseDownItem={this.onMouseDownItem}
+      onClickHeaderCell={this.onClickHeaderCell}
+      onMouseEnterItem={onMouseEnterItem}
+      onMouseLeaveItem={onMouseLeaveItem}
+      onDoubleClickItem={onDoubleClickItem}
+      onClickItem={() => {/* do nothing */ }}
+      sortingFlag={sortingFlag}
+      toolbar={this.toolbar}
+      items={items}
+      columns={columns} />
   }
 }
--- src/DirectoryTbl/__test__/DirectoryTbl.test.tsx
+++ src/DirectoryTbl/__test__/DirectoryTbl.test.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import {DirectoryTbl, IDirectoryTblProps} from './../DirectoryTbl';
-import {StringFldCell, CheckboxFldCell, ComboboxFldCell, typeColumn} from './../../Grid';
-import { shallow, mount} from 'enzyme';
-import {GridLayout} from './../../Grid';
-import {ItemsLayout} from './../../Grid/Items';
-import {HeaderLayout} from './../../Grid/Header';
-import {HeaderCellLayout} from './../../Grid/Header/HeaderCellLayout';
-import {ToolbarLayout} from './../../Grid/Toolbar'
-import {RowLayout} from '../../Grid/Items/RowLayout';
-import {HeaderRowLayout} from './../../Grid/Header/HeaderRowLayout';
-import {CellLayout} from './../../Grid/Items/CellLayout'
-import {columns, items} from './setUp'
+import { DirectoryTbl, IDirectoryTblProps } from './../DirectoryTbl';
+import { StringFldCell, CheckboxFldCell, ComboboxFldCell, typeColumn, typeItem } from './../../Grid';
+import { shallow, mount } from 'enzyme';
+import { GridLayout } from './../../Grid';
+import { ItemsLayout } from './../../Grid/Items';
+import { HeaderLayout } from './../../Grid/Header';
+import { HeaderCellLayout } from './../../Grid/Header/HeaderCellLayout';
+import { ToolbarLayout } from './../../Grid/Toolbar'
+import { RowLayout } from '../../Grid/Items/RowLayout';
+import { HeaderRowLayout } from './../../Grid/Header/HeaderRowLayout';
+import { CellLayout } from './../../Grid/Items/CellLayout'
+import { columns, items } from './setUp'
 
-import { StringFldLayoutEdit, FieldLayoutEdit, InputLayout , ImgButtonLayout} from '../../StringFld';
+import { StringFldLayoutEdit, FieldLayoutEdit, InputLayout, ImgButtonLayout } from '../../StringFld';
 
 const SEL_ROW_NUM = 0;
 const CELL_LOGIN_NUM = 1;
@@ -31,20 +31,20 @@ describe('DirectoryTbl', () => {
   });
 
   it('should created successfully', () => {
-    const component = mount(<DirectoryTbl columns = {columns}  />);
+    const component = mount(<DirectoryTbl columns={columns} />);
     expect(component.render()).toMatchSnapshot();
   });
 
 
   it('items property', () => {
-    const component = mount(<DirectoryTbl items={items} columns = {columns} />);
+    const component = mount(<DirectoryTbl items={items} columns={columns} />);
     expect(component.render()).toMatchSnapshot();
   });
 
 
   test('click add btn', () => {
     // add
-    const onChange =  jest.fn((newItems) => {
+    const onChange = jest.fn((newItems) => {
       expect(newItems.length).toBe(items.length + 1)
     })
     const component = mount(<DirectoryTbl items={items} onChange={onChange} columns={columns} />);
@@ -54,8 +54,8 @@ describe('DirectoryTbl', () => {
   })
 
 
-  test('click remove btn',()=>{
-    const onChange =  jest.fn((newItems) => {
+  test('click remove btn', () => {
+    const onChange = jest.fn((newItems) => {
       expect(newItems.length).toBe(items.length - 1)
     })
     const component = mount(<DirectoryTbl items={items} onChange={onChange} columns={columns} />);
@@ -66,25 +66,41 @@ describe('DirectoryTbl', () => {
   });
 
   test('select row & change val', () => {
-
-
-
-    const onChange =  jest.fn((newItems) => {
+    const onChange = jest.fn((newItems) => {
       expect(newItems[SEL_ROW_NUM].data.login).toBe('blabla')
     });
 
-    const onSelectItem =  jest.fn((selRow) => {
+    const onSelectItem = jest.fn((selRow) => {
       expect(selRow.data.login).toBe('Jane')
     });
 
     const component = mount(<DirectoryTbl onSelectItem={onSelectItem} items={items} onChange={onChange} columns={columns} />);
     component.find(RowLayout).at(SEL_ROW_NUM).find(CellLayout).at(CELL_LOGIN_NUM).find(StringFldCell).find('td').simulate('mousedown');
     expect(onSelectItem).toHaveBeenCalled();
-    component.find(RowLayout).at(SEL_ROW_NUM).find(CellLayout).at(CELL_LOGIN_NUM).find('input').simulate('change', {target: {value: 'blabla'}});
+    component.find(RowLayout).at(SEL_ROW_NUM).find(CellLayout).at(CELL_LOGIN_NUM).find('input').simulate('change', { target: { value: 'blabla' } });
     expect(onChange).toHaveBeenCalled();
   })
 
-  test('click header cell (sorting)',() => {
+
+  test('onMouseEnterItem & onMouseLeaveItem & onDoubleClickItem works fine', () => {
+    const onMouseEnterItem = jest.fn((rowObject: typeItem, cellAlias: string) => { });
+    const onMouseLeaveItem = jest.fn((rowObject: typeItem, cellAlias: string) => { })
+    const onDoubleClickItem = jest.fn((rowObject: typeItem, cellAlias: string) => { })
+    const component = mount(<DirectoryTbl onDoubleClickItem={onDoubleClickItem} onMouseEnterItem={onMouseEnterItem}
+      onMouseLeaveItem={onMouseLeaveItem} items={items} columns={columns} />);
+
+    component.find(RowLayout).at(SEL_ROW_NUM).find(CellLayout).at(CELL_LOGIN_NUM).find(StringFldCell).find('td').simulate('mouseenter');
+    expect(onMouseEnterItem).toHaveBeenCalled();
+
+    component.find(RowLayout).at(SEL_ROW_NUM).find(CellLayout).at(CELL_LOGIN_NUM).find(StringFldCell).find('td').simulate('mouseleave');
+    expect(onMouseLeaveItem).toHaveBeenCalled();
+
+    component.find(RowLayout).at(SEL_ROW_NUM).find(CellLayout).at(CELL_LOGIN_NUM).find(StringFldCell).find('td').simulate('dblclick');
+    expect(onDoubleClickItem).toHaveBeenCalled();
+  });
+
+
+  test('click header cell (sorting)', () => {
     const component = mount(<DirectoryTbl items={items} columns={columns} />);
     component.find(".st-grid-head-cell-textContainer").at(CELL_LOGIN_NUM).simulate('click');
     expect(component.find(".st-grid-head-cell-sortAnchor")).toHaveLength(1);
@@ -96,46 +112,45 @@ describe('DirectoryTbl', () => {
   })
 
 
-  test('change header cell width',() => {
-    const component = mount(<DirectoryTbl items={items} columns={columns} />,{ attachTo: container });
-    component.find(".st-grid-head-cell-widthChangeAnchor").at(CELL_LOGIN_NUM).simulate('mousedown',{pageX: 200});
+  test('change header cell width', () => {
+    const component = mount(<DirectoryTbl items={items} columns={columns} />, { attachTo: container });
+    component.find(".st-grid-head-cell-widthChangeAnchor").at(CELL_LOGIN_NUM).simulate('mousedown', { pageX: 200 });
     expect(component.render()).toMatchSnapshot();
- 
 
-    let clientX  = 200
-    for(let i = 0; i < 10; i ++)
-    {
+
+    let clientX = 200
+    for (let i = 0; i < 10; i++) {
       clientX = clientX + 20;
       let ev = new window.MouseEvent("mousemove", {
         bubbles: true,
         cancelable: true,
       });
-      Object.defineProperty(ev, 'pageX', {get: () => clientX});
+      Object.defineProperty(ev, 'pageX', { get: () => clientX });
       component.getDOMNode().dispatchEvent(ev);
-   }
+    }
 
     window.document.dispatchEvent(new MouseEvent("mouseup"));
     expect(component.render()).toMatchSnapshot();
   });
 
 
-  test('onmousewhell & scrolls on items',()=>{
-    const component = mount(<DirectoryTbl items={items.concat(items, items,items)}  columns={columns} />);
+  test('onmousewhell & scrolls on items', () => {
+    const component = mount(<DirectoryTbl items={items.concat(items, items, items)} columns={columns} />);
     let wr = component.find('.st-grid-body-div');
-    wr.simulate('wheel',{deltaY: 200})
-    wr.simulate('wheel',{deltaY: -100})
-    wr.simulate('scroll',{deltaY: 200})
-    wr.simulate('scroll',{deltaY: -100})
+    wr.simulate('wheel', { deltaY: 200 })
+    wr.simulate('wheel', { deltaY: -100 })
+    wr.simulate('scroll', { deltaY: 200 })
+    wr.simulate('scroll', { deltaY: -100 })
     wr = component.find('.st-grid-rightScroll-div');
-    wr.simulate('scroll',{deltaY: 200})
-    wr.simulate('scroll',{deltaY: -100})
+    wr.simulate('scroll', { deltaY: 200 })
+    wr.simulate('scroll', { deltaY: -100 })
 
     wr = component.find('.st-grid-bottomScroll-div');
-    wr.simulate('scroll',{deltaY: 200})
-    wr.simulate('scroll',{deltaY: -100})
-    
+    wr.simulate('scroll', { deltaY: 200 })
+    wr.simulate('scroll', { deltaY: -100 })
+
   });
 
 
-  
- })
+
+})
